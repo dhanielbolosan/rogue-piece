2ae91989de131d4188d35d559aba95f32eec3525
dhanielbolosan <dhaniel808123@gmail.com>
2025-01-16 23:11:02 -1000

    Object Classes for moves, items, and enemy + learn moves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-Object for a character
+Object class for a character
 TODO:
     1. Classes / Crew Position
         - Fighter
@@ -18,7 +18,7 @@ TODO:
     endurance, strength, health, agility
 '''
 class Character:
-    def __init__(self, name, health, strength, defense, stamina, level, experience, berries, statpoints, moves, items):
+    def __init__(self, name, health, strength, defense, stamina, level, experience, berries, bounty, statpoints, moves=None, items=None):
         self._name = name
         self._health = health
         self._strength = strength
@@ -28,8 +28,9 @@ class Character:
         self._experience = experience
         self._berries = berries
         self._statpoints = statpoints
-        self._moves = []
-        self.items = []
+        self._bounty = bounty
+        self._moves = moves if moves else []
+        self._items = items if items else []
 
         # stats dictionary
         self.stats = {
@@ -41,7 +42,10 @@ class Character:
             "Level": self.level,
             "Experience": self.experience,
             "Berries": self.berries,
+            "Bounty": self.bounty,
             "Stat Points": self.statpoints,
+            "Moves": self.moves,
+            "Items": self.items
         }
 
     # Getters
@@ -76,6 +80,10 @@ class Character:
     @property
     def berries(self):
         return self._berries
+    
+    @property
+    def bounty(self):
+        return self._bounty
         
     @property
     def statpoints(self):
@@ -122,6 +130,10 @@ class Character:
     def berries(self, berries):
         self._berries = berries
 
+    @bounty.setter
+    def bounty(self, bounty):
+        self._bounty = bounty
+
     @statpoints.setter
     def statpoints(self, statpoints):
         self._statpoints = statpoints
@@ -135,6 +147,24 @@ class Character:
         self._items = items
 
     '''
+    Add move
+    '''
+    def learn_move(self, move):
+        self.moves.append(move)
+
+    '''
+    Add item
+    '''
+    def add_item(self, item):
+        self.items.append(item)
+
+    '''
+    Take damage
+    '''
+    def take_damage(self, damage):
+        self.health -= damage
+
+    '''
     Prints character's stats
     Able to choose which stats to display
     '''
@@ -145,7 +175,20 @@ class Character:
         print(f"\n{self.name}'s stats:")
         for key in display_stats:
             if key in self.stats:
-                print(f"{key}: {self.stats[key]}")
+                if key == "Moves":
+                    if len(self.stats[key]) == 0:
+                        print(f"{key}: None")
+                        continue
+                    move_names = [move.name for move in self.stats[key]]
+                    print(f"{key}: {', '.join(move_names)}")
+                elif key == "Items":
+                    if len(self.stats[key]) == 0:
+                        print(f"{key}: None")
+                        continue
+                    item_names = [item.name for item in self.stats[key]]
+                    print(f"{key}: {', '.join(item_names)}")
+                else:    
+                    print(f"{key}: {self.stats[key]}")
 
     '''
     Updates a character's stats
@@ -214,16 +257,133 @@ class Character:
 
         name = input("Enter your character name: ")
 
-        character = Character(name, 10, 10, 10, 10, 1, 0, 0, 10, [], [])
+        character = Character(name, 10, 10, 10, 10, 1, 0, 0, 0, 10, [], [])
 
         character.print_stats(["Health", "Strength", "Defense", "Stamina"])
 
         character.upgrade_stats()
 
         character.print_stats()
-        
-'''
 
+'''
+Object class for an Enemy
+TODO:
+    1. Different types of enemies
+'''
+class Enemy:
+    def __init__(self, name, health, strength, defense, stamina, level, berries, moves, items):
+        self._name = name
+        self._health = health
+        self._strength = strength
+        self._defense = defense
+        self._stamina = stamina
+        self._level = level
+        self._berries = berries
+        self._moves = moves
+        self._items = items
+
+    # Getters
+    @property
+    def name(self):
+        return self._name
+        
+    @property
+    def health(self):
+        return self._health
+        
+    @property
+    def strength(self):
+        return self._strength
+        
+    @property
+    def defense(self):
+        return self._defense
+        
+    @property
+    def stamina(self):
+        return self._stamina
+        
+    @property
+    def level(self):
+        return self._level
+        
+    @property
+    def berries(self):
+        return self._berries
+    
+    @property
+    def moves(self):
+        return self._moves
+    
+    @property
+    def items(self):
+        return self._items
+    
+    # Setters
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @health.setter
+    def health(self, health):
+        self._health = health
+
+    @strength.setter
+    def strength(self, strength):
+        self._strength = strength
+        
+    @defense.setter
+    def defense(self, defense):
+        self._defense = defense
+
+    @stamina.setter
+    def stamina(self, stamina):
+        self._stamina = stamina
+
+    @level.setter
+    def level(self, level):
+        self._level = level
+        
+    @berries.setter
+    def berries(self, berries):
+        self._berries = berries
+
+    @moves.setter
+    def moves(self, moves):
+        self._moves = moves
+    
+    @items.setter
+    def moves(self, items):
+        self._items = items
+        
+
+'''
+Object class for Moves
+'''
+class Move:
+    def __init__(self, name, damage, stamina, description):
+        self.name = name
+        self.damage = damage
+        self.stamina = stamina
+        self.description = description
+
+    def __repr__(self):
+        return f"Move('{self.name}')"
+    
+    def use(self, target):
+        target.take_damage(self.damage)
+
+'''
+Object class for Items
+'''
+class Item:
+    def __init__(self, name, type, effect, description):
+        self._name = name
+        self._type = type
+        self._effect = effect
+        self._description = description
+
+'''
 Starting a new game will make you create a character and play the game
 TODO:
     1. Levels
@@ -234,7 +394,7 @@ def start():
     print("Starting Game")
     print("\n*************************\n")
 
-    user = Character("", 0, 0, 0, 0, 0, 0, 0, 0, [], [])
+    user = Character("", 0, 0, 0, 0, 0, 0, 0, 0, 0, [], [])
     user.character_creation()
 
 '''
@@ -286,6 +446,25 @@ def main():
                 print("ERROR: Enter a valid choice!")
         except ValueError:
             print("ERROR: Enter a number!")
-
+    
 if __name__ == "__main__":
     main()
+
+'''
+Test for adding moves
+print("Testing Game\n")
+
+    punch = Move("Punch", 5, 1, "A simple punch")
+    kick = Move("Kick", 10, 2, "A simple kick")
+
+    user = Character("Dummy", 10, 10, 10, 10, 1, 0, 0, 0, 10, [], [])
+
+    print("Before adding Moves")
+    user.print_stats()
+
+    user.learn_move(punch)
+    user.learn_move(kick)
+
+    print("\nAfter adding Moves")
+    user.print_stats()
+'''
